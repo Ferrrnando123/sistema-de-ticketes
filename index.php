265ecf7aba51f79e3d7016769edb6606a2464cc0
Alfaro134 <at251699@alumno.udb.edu.sv>
2026-02-22 16:12:54 -0600

    Cambios en index

--- index.php
+++ index.php
@@ -27,36 +27,42 @@ switch ($action) {
         break;
 
     case 'dashboard':
-        if (!isset($_SESSION['id'])) {
-            header("Location: index.php?action=login");
-            exit();
-        }
+        validarSesion();
         require 'views/dashboard.php';
         break;
 
-    // NUEVAS RUTAS AGREGADAS
     case 'nuevo-ticket':
-        if (!isset($_SESSION['id'])) {
-            header("Location: index.php?action=login");
-            exit();
-        }
+        validarSesion();
         require 'views/nuevo-ticket.php';
         break;
 
     case 'mis-tickets':
-        if (!isset($_SESSION['id'])) {
-            header("Location: index.php?action=login");
-            exit();
-        }
+        validarSesion();
         require 'views/mis-tickets.php';
+        break;
+
+    // ESTA ES LA RUTA QUE FALTABA PARA EL NAV
+    case 'panel-soporte':
+        validarSesion();
+        require 'views/panelsoporte.php'; 
         break;
 
     case 'logout':
         session_destroy();
         header("Location: index.php?action=login");
+        exit();
         break;
 
     default:
+        http_response_code(404);
         echo "404 - Esta página no existe en la UDB, pa";
         break;
+}
+
+// Función auxiliar para no repetir código en cada caso
+function validarSesion() {
+    if (!isset($_SESSION['id'])) {
+        header("Location: index.php?action=login");
+        exit();
+    }
 }
